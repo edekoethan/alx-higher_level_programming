446a52bcf0d03fca50dd30e82837e885e20ebc5c
edeko ethan <eromoseleedeko99@gmail.com>
2023-06-12 13:20:49 -0700

    3-print_alphabt.py redo

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
-
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if chr(letter) is not 'q' and chr(letter) is not 'e':
-        print("{}".format(chr(letter)), end="")
+for item in range(97, 123):
+    if item != 101 and item != 113:
+        print("{}".format(chr(item)), end="")
