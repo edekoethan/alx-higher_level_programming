10032cc2c35ec942e6afbc1d1aff9f06d120ec5c
edeko ethan <eromoseleedeko99@gmail.com>
2023-06-12 13:24:08 -0700

    5 redo

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for new_ number in range(0, 100):
-    if new_number == 99:
-        print("{}".format(new_number))
+for numbers in range(100):
+    if (numbers == 99):
+        print("{0:02}".format(numbers))
     else:
-        print("{:02}".format(new_number), end=", ")
+        print("{0:02}, ".format(numbers), end="")
