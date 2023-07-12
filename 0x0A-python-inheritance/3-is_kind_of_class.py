7b1aaa78635b4d86f10cd9ce85fce23c14571520
edeko ethan <eromoseleedeko99@gmail.com>
2023-07-12 06:41:28 -0700

    3, 5 redo

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-if the object is an instance of retyrn true, or if the object is an instance of a class that inherited from the specified class return false
+module with method is_kind_of_class
 """
 
 
