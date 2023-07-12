9efc5198b0a659bf76a1c20a599ec41248003027
edeko ethan <eromoseleedeko99@gmail.com>
2023-07-12 13:58:15 -0700

    task 0 redo

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""Defines an object attribute lookup function."""
 
 
-def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+def lookup(obj):
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
