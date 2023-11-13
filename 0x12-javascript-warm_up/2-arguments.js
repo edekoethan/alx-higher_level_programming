677bcb1c2ed18c81e934987737f294545c50e308
Edeko Eromosele <eromoseleedeko@gmail.com>
2023-11-13 12:02:24 +0100

    task 1-7

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,65 @@
+if (process.argv.length < 3) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+2-arguments.js [unix] (00:59 01/01/1970)                                                   0,0-1 All
+"2-arguments.js" [New]
+
